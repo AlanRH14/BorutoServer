7bd315a7f548f43785accca05ae9478a27daf1e8
AlanRH14 <alanarh.14@gmail.com>
2023-11-15 01:22:20 -0600

    Create root end point

---
+++ src/main/kotlin/com/aarh/routes/Root.kt
@@ -0,0 +1,15 @@
+package com.aarh.routes
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.root() {
+    get("/") {
+        call.respond(
+            message = "Welcome to Boruto API!",
+            status = HttpStatusCode.OK,
+        )
+    }
+}
