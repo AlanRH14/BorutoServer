7e920ed9720b571085a10c5026352a5d48009114
AlanRH14 <alanarh.14@gmail.com>
2023-11-16 01:12:07 -0600

    Create AllHeroes Endpoint - Part 2

--- src/main/kotlin/com/aarh/plugins/Routing.kt
+++ src/main/kotlin/com/aarh/plugins/Routing.kt
@@ -3,11 +3,13 @@ package com.aarh.plugins
 import com.aarh.routes.getAllHeroes
 import com.aarh.routes.root
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         root()
         getAllHeroes()
+        staticResources(remotePath = "/", basePackage = "", index = "image")
     }
 }
--- src/main/kotlin/com/aarh/routes/AllHeroes.kt
+++ src/main/kotlin/com/aarh/routes/AllHeroes.kt
@@ -1,19 +1,28 @@
 package com.aarh.routes
 
 import com.aarh.models.ApiResponse
+import com.aarh.repository.HeroRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 import java.lang.IllegalArgumentException
 import java.lang.NumberFormatException
 
 fun Route.getAllHeroes() {
+    val heroRepository: HeroRepository by inject()
+
     get("/boruto/heroes") {
         try {
             val page = call.request.queryParameters["page"]?.toInt() ?: 1
             require(page in 1..5)
-            call.respond(message = page)
+
+            val apiResponse = heroRepository.getAllHeroes(page)
+            call.respond(
+                message = apiResponse,
+                status = HttpStatusCode.OK,
+            )
         } catch (e: NumberFormatException) {
             call.respond(
                 message = ApiResponse(
