c9eecccb11aac58e6840b4efc682fd0ab064ada4
Alan Andres Rosas Hernandez <alanarh.14@gmail.com>
2023-11-17 01:03:41 -0600

    Testing root endpoint - Part 2

--- src/main/kotlin/com/aarh/plugins/StatusPages.kt
+++ src/main/kotlin/com/aarh/plugins/StatusPages.kt
@@ -9,7 +9,7 @@ fun Application.configurationStatusPages() {
     install(StatusPages) {
         status(HttpStatusCode.NotFound) { call, status ->
             call.respond(
-                message = "404: Page not Found",
+                message = "404: Page not Found.",
                 status = status,
             )
         }
