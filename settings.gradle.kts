0efc5382324a94acf28b0b9a9fc42ebd07d39d02
Alan Andres Rosas Hernández <alanarh.14@gmail.com>
2025-05-17 02:53:04 -0600

    Change final kobweb part. 1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,30 @@
-rootProject.name = "com.aarh.borutoserver"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+// The following block registers dependencies to enable Kobweb snapshot support. It is safe to delete or comment out
+// this block if you never plan to use them.
+gradle.settingsEvaluated {
+    fun RepositoryHandler.kobwebSnapshots() {
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") {
+            content { includeGroupByRegex("com\\.varabyte\\.kobweb.*") }
+            mavenContent { snapshotsOnly() }
+        }
+    }
+
+    pluginManagement.repositories { kobwebSnapshots() }
+    dependencyResolutionManagement.repositories { kobwebSnapshots() }
+}
+
+rootProject.name = "borutoserver"
+
+include(":site")
